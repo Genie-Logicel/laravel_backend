443164b5eacac2a23ef09d472813d0d62a774efa
TiavinaCyri <tiavinacyri@gmail.com>
2023-02-02 23:28:05 +0300

    success

--- config/services.php
+++ config/services.php
@@ -30,5 +30,10 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'twilio' => [
+        'account_sid' => 'AC5b1e6a6c7cd251f6bfc452e863555e3e',
+        'auth_token' => '967669a5ed63cfe42441a6918a8c0122',
+    ],
+    
 
 ];
